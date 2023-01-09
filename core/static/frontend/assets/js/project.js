fe15c174d87a7138204cb612f9009d22f2323a38
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2023-01-09 10:44:22 +0600

    updated project page

--- core/static/frontend/assets/js/project.js
+++ core/static/frontend/assets/js/project.js
@@ -2,21 +2,30 @@ const asterisk_img = document.querySelector(".asterisk-img");
 const categories = document.querySelectorAll("#pills-home-tab");
 
 function addAsterisk(target) {
+  console.log(categories[0].children[0].children[0].children[2].children[1]);
+  console.log(target);
   for (let i = 0; i < categories.length; i++) {
     if (
       target === categories[i] &&
-      categories[i].children[0].children[1].children[1].children.length === 0
+      categories[i].children[0].children[0].children[2].children[1].children
+        .length === 0
     ) {
+      console.log(1);
       var tag = document.createElement("img");
-      tag.src = "./assets/images/star_1.png";
-      categories[i].children[0].children[1].children[1].appendChild(tag);
+      tag.src = "/static/frontend/assets/images/star_1.png";
+      categories[i].children[0].children[0].children[2].children[1].appendChild(
+        tag
+      );
     }
+
     if (
-      target === categories[i] &&
-      categories[i].children[0].children[1].children[1].children.length === 1
+      target == categories[i] &&
+      categories[i].children[0].children[0].children[2].children[1].children
+        .length === 1
     ) {
     } else {
-      categories[i].children[0].children[1].children[1].innerHTML = "";
+      categories[i].children[0].children[0].children[2].children[1].innerHTML =
+        "";
     }
   }
 }
